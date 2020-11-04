8f25c4de6890262cb9c745698183eb6a22c28986
valavila <64001419+valavila@users.noreply.github.com>
2020-11-04 05:16:05 -0800

    Issue#2 customize identity (#18)
    * Scaffolded all ASP.NET Identity pages
    
    * Simplified name of database
    
    * Changed required and updated password requirements
    
    * Allowed user to sign in using username and added username field to register page
    
    * Change log in to require username instead of email
    
    * Remove external login providers from login page

--- ShowcaseSite/Startup.cs
+++ ShowcaseSite/Startup.cs
@@ -12,6 +12,7 @@ using ShowcaseSite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShowcaseSite.Models;
 
 namespace ShowcaseSite
 {
@@ -30,11 +31,13 @@ namespace ShowcaseSite
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
+        
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
